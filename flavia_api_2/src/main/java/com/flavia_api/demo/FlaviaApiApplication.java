85c92c176aa4d6ba256162bd0cfcd0aa654a82fd
Renata Emanuelle <renata.anhon@gmail.com>
2019-10-19 16:10:03 -0300

    projeto corrigido

---
+++ flavia_api_2/src/main/java/com/flavia_api/demo/FlaviaApiApplication.java
@@ -0,0 +1,13 @@
+package com.flavia_api.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlaviaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlaviaApiApplication.class, args);
+    }
+
+}
---
+++ flavia_api_2/src/test/java/com/flavia_api/demo/FlaviaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flavia_api.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlaviaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
