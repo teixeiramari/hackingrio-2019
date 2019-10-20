22f7fca90d49fd4706ed386712c5e10100240c39
TreicyMadelon <37406442+TreicyMadelon@users.noreply.github.com>
2019-10-20 00:01:48 -0300

    conflitos

--- flavia/src/app/app.module.ts
+++ flavia/src/app/app.module.ts
@@ -2,12 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-=======
 import { ConsultaComponent } from './consulta/consulta.component';
 import { TimeComponent } from './time/time.component';
-import { AngularFontAwesomeModule } from 'angular-font-awesome';
->>>>>>> 4c71c4cecc975ad408bcc37b43c7ec3565fbeb7a
 
 @NgModule({
   declarations: [
