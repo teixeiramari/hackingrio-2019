0fad95bba0136a6bf34cf874aee2104265e0e4ce
Renata Emanuelle <renata.anhon@gmail.com>
2019-10-20 00:12:19 -0300

    classes acrescentadas

---
+++ flavia_api_2/src/main/java/com/flavia_model/Time.java
@@ -0,0 +1,13 @@
+package com.flavia_model;
+
+import java.util.List;
+
+
+public class Time {
+
+    List<Atleta> atletas;
+
+
+
+
+}
---
+++ flavia_api_2/src/main/java/com/flavia_services/FlaviaService.java
@@ -0,0 +1,25 @@
+package com.flavia_services;
+
+import com.flavia_model.Jogo;
+import com.flavia_model.JogosTeste;
+
+import java.util.List;
+import java.util.Optional;
+
+public class FlaviaService {
+
+    /*
+
+    JogosTeste jogosTeste = new JogosTeste();
+
+
+    public Optional<Jogo> findById(Long idFormulario){
+        return formularioRepository.findById(idFormulario);
+    }
+
+    public List<Jogo> findAll(){
+        return formularioRepository.findAll();
+    }
+
+*/
+}
