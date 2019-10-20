d73b4e19449c22d2e4ec51199cdf97e013e742d5
rafaelaport <rife-rafe@hotmail.com>
2019-10-19 21:37:37 -0300

    adicionando telas novas

--- flavia/src/app/app.module.ts
+++ flavia/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConsultaComponent } from './consulta/consulta.component';
+import { TimeComponent } from './time/time.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConsultaComponent,
+    TimeComponent
   ],
   imports: [
     BrowserModule,
